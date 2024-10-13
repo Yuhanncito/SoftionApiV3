36f61e8d2d9cefbfabaf1c2e029ffdeb6519650c
Yuhanncito <uchijaisuka02@gmail.com>
2024-10-13 13:45:39 -0600

    commit with PUSH NOTIFICATION TEST v5

--- src/libs/socketServer.js
+++ src/libs/socketServer.js
@@ -8,19 +8,7 @@ class Socket {
    * @param {http.Server} AppServer - The express app server
    */
   constructor(AppServer) {
-    const allowedOrigins = ['http://localhost:4173', 'http://localhost:5173','https://softion-pro-dist.vercel.app'];
-    Socket.#server = socketIo(AppServer, {
-      cors: {
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-              callback(null, true);
-            } else {
-              callback(new Error('No permitido por CORS'));
-            }
-          },
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-      },
-    });
+    Socket.#server = socketIo(AppServer);
   }
   getServer() {
     return Socket.#server;
