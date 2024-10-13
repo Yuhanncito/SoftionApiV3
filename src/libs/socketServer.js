aa5abd634cd77d5f6d458b79339333eb46798e77
Yuhanncito <uchijaisuka02@gmail.com>
2024-10-13 02:50:46 -0600

    commit with PUSH NOTIFICATION TEST v4

--- src/libs/socketServer.js
+++ src/libs/socketServer.js
@@ -1,30 +1,36 @@
 import socketIo from "socket.io";
 
 class Socket {
-    static #server;
-    static #sockets;
-    /**
-     * Constructor for the Socket class
-     * @param {http.Server} AppServer - The express app server
-     */
-    constructor(AppServer){
-        Socket.#server = socketIo(AppServer, {
-            cors: {
-                origin: "*",
-                methods: ["GET", "POST", "PUT", "DELETE",'PATCH'],
+  static #server;
+  static #sockets;
+  /**
+   * Constructor for the Socket class
+   * @param {http.Server} AppServer - The express app server
+   */
+  constructor(AppServer) {
+    const allowedOrigins = ['http://localhost:4173', 'http://localhost:5173','https://softion-pro-dist.vercel.app'];
+    Socket.#server = socketIo(AppServer, {
+      cors: {
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+              callback(null, true);
+            } else {
+              callback(new Error('No permitido por CORS'));
             }
-        });
-    }
-    getServer(){
-        return Socket.#server;
-    }
-    getSocket(){
-        return Socket.#sockets;
-    }
-    setSocket(socket){
-        Socket.#sockets = socket;
-    }
+          },
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+      },
+    });
+  }
+  getServer() {
+    return Socket.#server;
+  }
+  getSocket() {
+    return Socket.#sockets;
+  }
+  setSocket(socket) {
+    Socket.#sockets = socket;
+  }
 }
-
 
 export default Socket;
