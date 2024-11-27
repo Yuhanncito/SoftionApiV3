09745f680bc37f9d7539ecdf41ffe7fd016cfcc9
Yuhanncito <uchijaisuka02@gmail.com>
2024-11-26 23:06:35 -0600

    validaciones

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -3,6 +3,9 @@ import Apps from "../models/app.model";
 export const saveData = async (req, res) => {
     try {
         const { url, nombre, icono } = req.body;
+        if (!url || !nombre || !icono) {
+            return res.status(400).json({ message: "Campos requeridos" });
+        }
         const newApp = new Apps({ url, nombre, icono });
         await newApp.save();
         res.status(200).json({ message: "ok" });
